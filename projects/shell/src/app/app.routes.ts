6facae5b21f8f70caaf1cdf96e2caaed26f26b10
Nacho Navarro Carter <lnavarro.carter@gmail.com>
2022-10-25 13:17:49 -0300

    Init MicrofrontEnd

--- projects/shell/src/app/app.routes.ts
+++ projects/shell/src/app/app.routes.ts
@@ -1,29 +1,36 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
-export const APP_ROUTES: Routes = [
-    {
-      path: '',
-      component: HomeComponent,
-      pathMatch: 'full'
-    },
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
 
+  {
+    path: 'flights',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './Module'
+      })
+      .then(m => m.FlightsModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4202/remoteEntry.js',
+        exposedModule: './Module'
+      })
+        .then(m => m.App2Module)
+  },
+  {path : '', redirectTo: 'home', pathMatch: 'full'},
+  {path : '**', component: NotFoundComponent}
+]
 
-    // Your route here:
-
-    // {
-    //   path: 'flights',
-    //   loadChildren: () => import('mfe1/Module').then(m => m.FlightsModule)
-    // },
-
-    {
-      path: '**',
-      component: NotFoundComponent
-    }
-
-    // DO NOT insert routes after this one.
-    // { path:'**', ...} needs to be the LAST one.
-
-];
-
+export const APP_ROUTES = routes;
--- projects/shell/src/decl.d.ts
+++ projects/shell/src/decl.d.ts
@@ -1,1 +1,2 @@
-// declare module 'mfe1/Module';
+declare module 'mfe1/Module';
+declare module 'operador/Module'
